143dc492a7c694bf6f8bd2b3543333d174684d5a
Richard Rothwell <rrothwell@v3.org.au>
2015-06-19 10:36:05 +1000

    Welcome Slideshow module with configuration and content loading. Adjustments to the template css for removing the tabs on the 3 feature panels. The button css is now moved from the HTML Learn module to the gvl.css.

--- mod_gvlwelcomeslide/tmpl/default.php
+++ mod_gvlwelcomeslide/tmpl/default.php
@@ -4,27 +4,63 @@ defined('_JEXEC') or die; ?>
 
 <?php
 
-	JHTML::stylesheet('gvl_welcome_slide.css', 'modules/mod_gvlwelcomeslide/css/');
+JHTML::stylesheet('gvl_welcome_slide.css', 'modules/mod_gvlwelcomeslide/css/');
+
+$javascript = <<<EOD
+    $(function(){
+      $("#slides").slidesjs({
+		width: 399,
+		height: 300,
+		play: {
+			active: false,
+			effect: "slide",
+			interval: 5000,
+			auto: true,
+			swap: true,
+			pauseOnHover: true,
+			restartDelay: 2500
+		},
+		pagination: {
+			active: false,
+			effect: "slide"
+		},
+		navigation: {
+			active: false,
+			effect: "slide"
+		}
+      });
+    });
+EOD;
+
+$document =& JFactory::getDocument();
+$document->addScript('/modules/mod_gvlwelcomeslide/js/jquery.slides.min.js');
+$document->addScriptDeclaration($javascript);
 	
-	// Not using the computed welcome stuff yet.
-	//echo $welcome;
+// Not using the computed welcome stuff yet.
+// echo $welcome;
 
 ?>
 
 <div id="gvl-welcome-slide" class="gvl-welcome">
-        <div class="gvl-heading">
-		  <h1>Genomics Virtual Lab</h1>
-		  <h2>A virtual laboratory for genomics research and training. </h2>
-        </div>
-        <div class="gvl-img-wrapper">
-		  <img src="<?php echo JURI::root();?>modules/mod_gvlwelcomeslide/tmpl/images/splash.jpg" alt="GVL Splash Image" />
+	<div class="gvl-heading">
+	  <h1>Genomics Virtual Lab</h1>
+	  <h2>A virtual laboratory for genomics research and training. </h2>
+	</div>
+	<div class="gvl-img-wrapper">
+		<div id="slides">
+			<img src="<?php echo JURI::root();?>modules/mod_gvlwelcomeslide/tmpl/images/splash0.jpg" alt="GVL Splash Image" />
+			<img src="<?php echo JURI::root();?>modules/mod_gvlwelcomeslide/tmpl/images/splash1.jpg" alt="GVL Splash Image" />
+			<img src="<?php echo JURI::root();?>modules/mod_gvlwelcomeslide/tmpl/images/splash2.jpg" alt="GVL Splash Image" />
+			<img src="<?php echo JURI::root();?>modules/mod_gvlwelcomeslide/tmpl/images/splash3.jpg" alt="GVL Splash Image" />
+			<img src="<?php echo JURI::root();?>modules/mod_gvlwelcomeslide/tmpl/images/splash4.jpg" alt="GVL Splash Image" />
 		</div>
-	    <div class="gvl-row" style="min-height: 1px;">
-		  <div class="gvl-col-md-6 gvl-col">
-		    <h4 class="gvl-introduction">
-		    <!-- Placeholder for additional content. -->
-			</h4>
-		  </div>
-	    </div class="gvl-row">
+	</div>
+	<div class="gvl-row" style="min-height: 1px;">
+	  <div class="gvl-col-md-6 gvl-col">
+		<h4 class="gvl-introduction">
+		<!-- Placeholder for additional content. -->
+		</h4>
+	  </div>
+	</div class="gvl-row">
 </div class="gvl_welcome">
 
